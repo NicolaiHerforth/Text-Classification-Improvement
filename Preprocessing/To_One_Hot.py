b00378f5e33fe0efcf59fdf45f35b74284662fd8
Nicolai Rendtslev <nre@caliodesign.com>
2019-03-22 17:56:56 +0100

    stash shit

--- Preprocessing/To_One_Hot.py
+++ Preprocessing/To_One_Hot.py
@@ -2,7 +2,7 @@ import pandas as pd
 from keras.preprocessing.text import text_to_word_sequence
 from collections import Counter
 
-def file_to_one_hot(data, test_data):
+def file_to_one_hot(data):
     corpus = set()
     data['summary'].apply(corpus.update)
     data['reviewText'].apply(corpus.update)
@@ -17,20 +17,9 @@ def file_to_one_hot(data, test_data):
         for i in range(len(l)):
             l[i] = words_to_indices[l[i]]
         return l
-
-    def convert_list_to_ints_for_test(l):
-        for i in range(len(l)):
-            if l[i] not in corpus:
-                l[i] = len(corpus)
-            else:
-                l[i] = words_to_indices[l[i]]
-        return l
-
-
-    test_data['summary'] = test_data['summary'].apply(convert_list_to_ints_for_test)
-    test_data['reviewText'] = test_data['reviewText'].apply(convert_list_to_ints_for_test)
+    print('part3')
 
     data['summary'] = data['summary'].apply(convert_list_to_ints)
     data['reviewText'] = data['reviewText'].apply(convert_list_to_ints)
-
-    return data, test_data
+    print('part4')
+    return data
