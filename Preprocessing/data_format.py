d22b342e9c9db8b8bd188426ee6b3e5b8f9bf9bb
Oscar Werner <oscarwerner@Oscars-MacBook-Pro.local>
2019-03-26 15:03:56 +0100

    your mom

--- Preprocessing/data_format.py
+++ Preprocessing/data_format.py
@@ -9,20 +9,22 @@ def formatting(path, prune = False):
     data['summary'] = data['summary'].apply(text_to_word_sequence)
     data['reviewText'] = data['reviewText'].apply(text_to_word_sequence)
 
-    def get_words_to_prune(d):
+    def get_words_to_prune(d, k = False):
         w_counts = Counter()
         d['summary'].apply(w_counts.update)
         d['reviewText'].apply(w_counts.update)
+
         return set([word for word in w_counts if w_counts[word] < 6])
-        
 
     def remove_pruned(words):
-        return ' '.join([word for word in words if word not in blacklist])
+        return [word for word in words if word not in blacklist]
         
     if prune:
         blacklist = get_words_to_prune(data)
         data['summary'] = data['summary'].apply(remove_pruned)
         data['reviewText'] = data['reviewText'].apply(remove_pruned)
-        
+
     return data
+
+
 
