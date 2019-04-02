9da66a8163666f1e32f13e81f66362171bbb6e17
Nicolai Rendtslev <nre@caliodesign.com>
2019-04-02 13:11:29 +0200

    fixed oh and naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,33 +4,34 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
 data = formatting('phase1_movie_reviews-train.csv', prune = True)
-# shuffle the dataset to make sure it's not ordered data
-data = data.sample(frac=1).reset_index(drop=True)
-# if you want to use only a percentage of the dataset, change below variable to whatever percentage you want to use
-data = data[:round(len(data)*.1)]
+
 train_data = data[:round(len(data)*.8)]
 val_data = data[round(len(data)*.8):]
 
-print('Converting data to dense OneHot encoding')
+print('\t Converting data to dense OneHot encoding')
 corpus = one_hot.get_corpus(train_data)
+max_len = (data['reviewText'].map(len)).max()
 
-train_data = one_hot.file_to_one_hot(train_data, corpus)
-val_data = one_hot.file_to_one_hot(val_data, corpus, test = True)
+train_data = one_hot.file_to_one_hot(train_data, corpus, pad_size= max_len)
+val_data = one_hot.file_to_one_hot(val_data, corpus, test = True, pad_size= max_len)
 
-print('Converted! ')
+print('\t Converted! ')
 
+training = train_data
+validation = val_data
 
-#X_train = (training.drop(['polarity'], axis=1)).values
-X_train = train_data['reviewText'].values
-y_train = train_data['polarity'].values
-X_val = val_data['reviewText'].values
-y_val = (val_data['polarity']).values
+X_train = training['reviewText'].values
+y_train = training['polarity'].values
+
+X_val = validation['reviewText'].values
+y_val = (validation['polarity']).values
 
 clf = MultinomialNB()
-
 clf.fit(list(X_train), y_train)
 
-y_pred_class = clf.predict(list(X_val))
+y_train_pred = clf.predict(list(X_train))
+y_val_pred = clf.predict(list(X_val))
 
 print('Validation Accuracy')
-print('  ',metrics.accuracy_score(y_val, y_pred_class))
+print('  ',metrics.accuracy_score(y_val, y_val_pred))
+
