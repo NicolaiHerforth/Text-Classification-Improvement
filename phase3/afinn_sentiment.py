181e56de9d919ecb66a1df0f1711fe4b76e7be7f
Oscar Werner <oscarwerner@Oscars-MacBook-Pro.local>
2019-05-02 12:11:21 +0200

    finished afinn

--- phase3/afinn_sentiment.py
+++ phase3/afinn_sentiment.py
@@ -1,19 +1,24 @@
 from afinn import Afinn
 
-def afinn_sentiment(sentence):
-    af = Afinn()
+
+def avg_afinn_sentiment(sentence, af):
     negations = set(['not', 'dont'])
     first = True
-    for i in range(len(sentence)):
+    print(sentence)
+    new_sent = []
+
+    for i,w in enumerate(sentence):
         if first:
-            sentence[i] = af.score(sentence[i])
+            new_sent.append(af.score(w))
             first = False
         else:
-            sentence[i] = af.score(sentence[i])
-            if sentence[i-1] in set(['not' or :
-                sentence[i] = -sentence[i]
+            new_sent.append(af.score(w))
+            if sentence[i-1] in negations:
+                new_sent[-1] = -new_sent[-1]
+
     
-    return sum(sentence)/len(sentence)
+    
+    return sum(new_sent)/len(new_sent)
 
 
 
---
+++ phase3/data_format_phase3.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from keras.preprocessing.text import text_to_word_sequence
+from collections import Counter
+from nltk.corpus import stopwords
+import numpy as np
+from afinn_sentiment import avg_afinn_sentiment
+from afinn import Afinn
+
+
+
+def formatting(path, prune = False):
+    af = Afinn()
+
+    def get_words_to_prune(d, k = False):
+
+        stop_words = set(stopwords.words('english'))
+        w_counts = Counter()
+        d['summary'].apply(w_counts.update)
+        d['reviewText'].apply(w_counts.update)
+
+        return set([word for word in w_counts if w_counts[word] < 10]) | stop_words
+
+    def remove_pruned(words):
+        return [word for word in words if word not in blacklist]
+
+    def print_head(df):
+        for _, row in df.head(5).iterrows():
+            print(row)
+
+
+    data = pd.read_csv(path)
+    data = data[:round(len(data)/10000)]
+
+    print_head(data)
+    data["summary"] = data["summary"].astype(str)
+    data['reviewText'] = data['reviewText'].astype(str)
+    data['summary'] = data['summary'].apply(text_to_word_sequence)
+    data['reviewText'] = data['reviewText'].apply(text_to_word_sequence)
+
+    #if prune:
+    #    blacklist = get_words_to_prune(data)
+    #    data['summary'] = data['summary'].apply(remove_pruned)
+    #    data['reviewText'] = data['reviewText'].apply(remove_pruned)
+
+    def forward(smth):
+        return avg_afinn_sentiment(smth,af)
+
+    #temp = data[reviewText]
+    print_head(data)
+
+    data['affin_score'] = data['reviewText'].apply(forward)
+
+    print_head(data)
+
+
+    return data
+
+path = '../phase1_movie_reviews-train.csv'
+
+formatting(path)
+
+
+
+
+
+
+
